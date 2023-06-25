9bb79f8412aa90d94175a0aa693de19152145584
FRIDRICHOVSKY Jan <jan.fridrichovsky@leica-geosystems.com>
2023-06-25 21:45:44 +0200

    Backup commit

---
+++ build_images/00_run_all.sh
@@ -0,0 +1,4 @@
+./01_build_samba.sh
+./02_build_arm64.sh
+./03_build_poky.sh
+./04_run_yocto.sh
--- build_images/04_run_yocto.sh
+++ build_images/04_run_yocto.sh
@@ -3,7 +3,6 @@
 docker volume create --name myvolume
 docker run -it --rm -v myvolume:/workdir busybox chown -R 1000:1000 /workdir
 docker create -t -p 445:445 --name samba -v myvolume:/workdir crops/samba:selfbuilt
-docker start samba
-sudo ifconfig lo0 127.0.0.2 alias up
 docker start samba && sudo ifconfig lo0 127.0.0.2 alias up
+#Open Finder, then hit 'Command-K'. In the "Server Address" box type smb://127.0.0.2/workdir and click "Connect".
 docker run --rm -it -v myvolume:/workdir crops/poky:selfbuilt --workdir=/workdir
